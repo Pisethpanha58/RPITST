b4659b442658ce79459318cf0b54561eab984005
Pisethpanha58 <yornpisethpanha72@gmail.com>
2024-09-25 13:38:29 +0700

    RPITST

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/flowbite/**/*.js",
+    "node_modules/preline/dist/*.js",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require("flowbite/plugin"),
+    require("preline/plugin"),
+    require("daisyui"),
+  ],
+};
